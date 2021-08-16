11037a6ec396f29a723a638be16da509c5268d9e
Dávid Slivka <david.slivka.sk@gmail.com>
2021-08-16 21:36:59 +0200

    Fixing a bug

--- digit_mnist.py
+++ digit_mnist.py
@@ -5,9 +5,10 @@ mnist = tf.keras.datasets.mnist
 
 x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
+print(x_test[0])
 
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten)
+model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dense(units=64, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(units=64, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax))
